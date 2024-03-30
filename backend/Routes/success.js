602cae2a7794bf5042586aa5582458e620524b1f
unknown <balasubramanian150304@gmail.com>
2024-03-30 17:46:35 +0530

    first commit

--- backend/Routes/success.js
+++ backend/Routes/success.js
@@ -40,7 +40,7 @@ router.post('/:id', async(req, res, next) => {
   const sig = req.headers['stripe-signature'];
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   let event;
-  const endpointSecret = 'whsec_7256d0bfddca3197b6eceb40a55b59582a12b4737409e52c56ec97dbbfe59604';
+  const endpointSecret = 'whsec_FnKD3fEh1sRJ3Vqrkff59oppgFJ8lmwC';
   
   try {
     event = stripe.webhooks.constructEvent(req.rawBody, sig, endpointSecret);
